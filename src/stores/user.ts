d5dc9d6cc4294bbc1b3218c94f9a6c586278f07b
prazdevs <sachabouillez@gmail.com>
2022-04-13 10:42:22 +0200

    add actions/getters

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -3,5 +3,15 @@ import { defineStore } from 'pinia'
 export const useUserStore = defineStore('user', {
   state: () => ({
     user: 'Sacha'
-  })
+  }),
+  actions: {
+    greetUser() {
+      console.log(this.userGreeting)
+    }
+  },
+  getters: {
+    userGreeting(state) {
+      return `Hello ${state.user}`
+    }
+  }
 })
